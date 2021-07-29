65bd3782bea4c529354490314bc11bb0260d63a4
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2021-07-28 21:47:09 -0400

    feat(pages): hook up faked cms content on the blogs landing page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -34,7 +34,22 @@ Blog.defaultProps = {
 
 export default Blog
 
-/**
- * Need to get the posts from the
- * fs and our CMS
- */
+export async function getStaticProps() {
+  const cmsPosts = postsFromCMS.published.map(post => {
+    const {data} = matter(post)
+    return data;
+  })
+
+  const postsPath = path.join(process.cwd(), 'posts');
+  const filenames = fs.readdirSync(postsPath);
+  const filePosts = filenames.map(name => {
+    const fullPath = path.join(process.cwd(), 'posts', name)
+    const file = fs.readFileSync(fullPath, 'utf-8');
+    const {data} = matter(file)
+    return data
+  })
+
+  const posts = [...cmsPosts, ...filePosts];
+
+  return { props: { posts } }
+}
